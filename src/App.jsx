b28cf0a487d4c3d3140365a9d19c1bb11bd1132c
MaccccD <Dumisanimofolo@gmail.com>
2025-08-01 11:48:00 +0200

    Revert "Main Pages Routing Set Up Part 1"
    This reverts commit 5ca4e95f245d09cd0baa4029bff0f0100c28544c.

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
 import HomePage from './pages/HomePage';
 import ChatPage from './pages/ChatPage';
-// import DashboardPage from './pages/DashboardPage';
+import DashboardPage from './pages/DashboardPage';
 import RewardsPage from './pages/RewardsPage';
-// import ProfilePage from './pages/ProfilePage';
-import MentalWellnessPage from './pages/MentalWellnessPage';
-import FinancialLiteracyPage from './pages/FinancialLiteracyPage';
+import ProfilePage from './pages/ProfilePage';
 import './index.css';
 
 function App() {
@@ -16,11 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="Wellness" element= {<MentalWellnessPage/>}/>
-          <Route path="Finance" element = {<FinancialLiteracyPage/>}/>
-          <Route path="Dumzii" element ={<ChatPage/>}/>
+          <Route path="chat" element={<ChatPage />} />
+          <Route path="dashboard" element={<DashboardPage />} />
           <Route path="rewards" element={<RewardsPage />} />
-        
+          <Route path="profile" element={<ProfilePage />} />
         </Route>
       </Routes>
     </Router>
