6608ef29b57a521f5515388231e20a7b71ff31b2
IvyC <ivy.chepkwony@gmail.com>
2025-08-02 13:01:52 +0200

    qr code simulator

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+
+// Your Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyDAlVzUeNuEYCnCWvg_v5rw80q9YXvysYc",
+  authDomain: "discovery-gradhack25jnb-108.firebaseapp.com",
+  projectId: "discovery-gradhack25jnb-108",
+  storageBucket: "discovery-gradhack25jnb-108.firebasestorage.app",
+  messagingSenderId: "1011454667050",
+  appId: "1:1011454667050:web:230f3a6ca09b9c5907d653"
+
+};
+
+
+// Initialize Firebase (if not already done)
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
