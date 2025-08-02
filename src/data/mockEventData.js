3f3061ee2b8d425918feb1c85ea852f0a45ceda6
Amyy0404 <anyvv9@gmail.com>
2025-08-02 09:04:28 +0200

    Home Page and D3

---
+++ src/data/mockEventData.js
@@ -0,0 +1,9 @@
+const mockEventData = [
+    { event: 1, Wits: 130, UP: 67, UCT: 99 },
+    { event: 2, Wits: 88,  UP: 59, UCT: 93 },
+    { event: 3, Wits: 165, UP: 120, UCT: 203 },
+    { event: 4, Wits: 203, UP: 178, UCT: 159 },
+  ];
+  
+  export default mockEventData;
+  
